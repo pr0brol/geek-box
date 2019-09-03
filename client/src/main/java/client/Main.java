dca419062a1e3782c15e106306d81a4f8bc341b8
pr0brol <pr0_brol@mail.ru>
2019-09-03 10:21:39 +0300

    problem

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,11 +1,20 @@
 package client;
 
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-
-public class Main extends Window {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        NettyClient nettyClient = new NettyClient();
+        Window window = new Window(nettyClient);
+        window.start(primaryStage);
+        nettyClient.run();
     }
 }
